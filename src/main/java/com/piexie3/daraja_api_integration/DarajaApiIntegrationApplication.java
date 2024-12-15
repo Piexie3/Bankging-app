697a8cdb61fc1b5fb12ab190cb4e86881f33a86f
piexie3 <emmanuelkbett3@gmail.com>
2024-12-15 23:27:14 +0300

    fixing auditing error

--- src/main/java/com/piexie3/daraja_api_integration/DarajaApiIntegrationApplication.java
+++ src/main/java/com/piexie3/daraja_api_integration/DarajaApiIntegrationApplication.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -29,6 +30,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				url = "https://github.com/piexie3"
 		)
 )
+@EnableJpaAuditing
 public class DarajaApiIntegrationApplication {
 
 	public static void main(String[] args) {
